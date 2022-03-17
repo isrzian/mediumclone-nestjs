68e4eb8b7a07dc32c6f6e8b251c146aa94b2ce02
isrzian <shadow-sergej@ya.ru>
2022-03-17 13:11:02 +0700

    connect to database

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from "typeorm"
+
+export const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: '123',
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+}
