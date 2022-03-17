59b022def7cd380bd45ca3ad316ceb932a9b489f
isrzian <shadow-sergej@ya.ru>
2022-03-17 14:04:06 +0700

    end tags

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { TagController } from "./tag.controller";
 import { TagService } from "./tag.service";
+import { TagEntity } from "./tag.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
-  providers: [TagService]
+  providers: [TagService],
 })
 export class TagModule {}
