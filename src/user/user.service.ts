87485d89ef08999e6c76ecf53255928b529d2c53
isrzian <shadow-sergej@ya.ru>
2022-03-19 16:06:45 +0700

    jwt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { sign } from 'jsonwebtoken';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
-import { Repository } from 'typeorm';
+import { JWT_SECRET } from '../config';
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Injectable()
 export class UserService {
@@ -15,5 +18,25 @@ export class UserService {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
